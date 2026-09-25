dc15607b2935e8cc493542bbe365bdcb0536efe0
Eltaf Sameem <140640813+Eltaf-Sameem@users.noreply.github.com>
2026-09-25 02:27:10 -0400

    updated main method TODO list

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -3,7 +3,9 @@ public class TopSecret {
         // TODO: Implement the main method
         // TODO: make sure we can run with gradle
         // TODO: make sure we can run from terminal
-        // TODO: make sure we can run using the generated JAR file
+        // TODO: make sure we can run using the generated fat JAR file
+        // TODO: make sure all test cases pass
+        // TODO: validate/double check application requirements and app functinallity
         System.out.println("I am in Main method");
     }
 }
