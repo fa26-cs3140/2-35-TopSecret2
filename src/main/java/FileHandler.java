042356186a330e1fc2a6fe50847503df2797a9ae
Eltaf Sameem <140640813+Eltaf-Sameem@users.noreply.github.com>
2026-09-24 22:24:42 -0400

    FileHandler init  Method stub and test

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -2,9 +2,21 @@ import java.util.ArrayList;
 
 public class FileHandler {
 
-    public static ArrayList<String> getFileLinesList(String dataFileName){
+    /**
+     * returns an ArrayList of the lines of the file
+     *
+     * @param fileName name of the file to read with its file extension
+     * @param fileType type of file to read: DATA or CIPHER from {@code enum fileTypes}
+     * @return {@code ArrayList<String>} containing the lines of the file. {@code null} if file does not exist
+     */
+    public static ArrayList<String> getFileLinesList(String fileName, fileTypes fileType) {
 
         return new ArrayList<>();
+    }
+
+    public enum fileTypes {
+        DATA,
+        CIPHER
     }
 
 
