be0d53f7486e9ffe9788d2e7e351b1fb70c32818
MrTwoMile <35352512+MrTwoMile@users.noreply.github.com>
2026-09-25 01:27:43 -0400

    Implement key in Cipher initialization

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,6 +1,47 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+
 public class Cipher {
+    private HashMap<Character, Character> substitutions;
+
+
+    public Cipher() {
+        // TODO: Implement exception
+
+        ArrayList<String> keyLines =
+                FileHandler.getFileLinesList(
+                        "key.txt",
+                        FileHandler.FileTypes.CIPHER
+                );
+
+        if (keyLines.size() != 2) {
+            // TODO: Implement exception
+        }
+
+        String regularCharacters = keyLines.get(0);
+        String cipherCharacters = keyLines.get(1);
+
+        if (regularCharacters.length() != cipherCharacters.length()) {
+            // TODO: Implement exception
+        }
+
+        int numKeyCharacters = regularCharacters.length();
+
+        substitutions = new HashMap<>(numKeyCharacters);
+
+        for (int characterIdx = 0;
+             characterIdx < numKeyCharacters;
+             ++characterIdx) {
+            substitutions.put(
+                    regularCharacters.charAt(characterIdx),
+                    cipherCharacters.charAt(characterIdx)
+            );
+        }
+    }
+
     public String decipher(String cipherText) {
         // TODO: Implement substitution cipher
+
         return cipherText;
     }
 
