02a820f567512029249aa6a09463c85151a8dfbf
Eltaf Sameem <140640813+Eltaf-Sameem@users.noreply.github.com>
2026-09-24 21:24:19 -0400

    added FileHandler and renamed cipher folder

---
+++ src/main/java/FileHandler.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class FileHandler {
+
+    public static ArrayList<String> getFileLinesList(String dataFileName){
+
+        return new ArrayList<>();
+    }
+
+
+}
